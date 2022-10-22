64d480a0ad7aa16e7fb5da53c576bef926cb401f
Noelite <52377212+Noelite@users.noreply.github.com>
2022-10-22 07:57:04 +0200

    1.4.0 => 1.5.1

--- AutoUpdate.py
+++ AutoUpdate.py
@@ -68,7 +68,7 @@ programs = {
 "processhacker": type("", (), {"name": "Process Hacker 2", "version": "", "ext": "exe"})
 }
 
-VERSION = "1.4.0"
+VERSION = "1.5.1"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
 
 
