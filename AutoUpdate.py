ecac4db43ebbf9e67c3f7738f126500a481685e4
Mathis Cavalli <mathiscavalli@gmail.com>
2025-02-28 19:27:23 +0100

    Bump version

--- AutoUpdate.py
+++ AutoUpdate.py
@@ -74,7 +74,7 @@ programs = {
 "bru": type("", (), {"name": "Bulk Rename Utility", "version": "", "ext": "exe"})
 }
 
-VERSION = "1.8.8"
+VERSION = "1.8.9"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 
 
